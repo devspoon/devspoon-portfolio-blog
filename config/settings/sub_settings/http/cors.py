f129ccc1800bf85c22c782d826f6112695fa22d9
bluebamus <bluebamus@naver.com>
2023-11-21 14:25:03 +0900

    update CORS_ORIGIN_WHITELIST at cors.py

--- config/settings/sub_settings/http/cors.py
+++ config/settings/sub_settings/http/cors.py
@@ -1,43 +1,41 @@
 import os
 import sys
+from decouple import config
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from  config.settings.base import INSTALLED_APPS, MIDDLEWARE
+from config.settings.base import INSTALLED_APPS, MIDDLEWARE
 
 # python -m pip install django-cors-headers
-'''
+"""
 https://oen-blog.tistory.com/46
 https://github.com/adamchainz/django-cors-headers
 
 * CORS_ALLOWED_ORIGINS : Previously this setting was called CORS_ORIGIN_WHITELIST, which still works as an alias, with the new name taking precedence.
-'''
+"""
 
 
-INSTALLED_APPS.insert(0,"corsheaders")
+INSTALLED_APPS.insert(0, "corsheaders")
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-]
+domain_str = config("DOMAIN_NAME")
+
+CORS_ORIGIN_WHITELIST = domain_str.split(",")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 
-CORS_ALLOW_METHODS = [
-    'DELETE', 'GET', 'OPTIONS', 'POST', 'PUT'
-]
+CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "POST", "PUT"]
 
 CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
-MIDDLEWARE.insert(0,"corsheaders.middleware.CorsMiddleware")
-
+MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
