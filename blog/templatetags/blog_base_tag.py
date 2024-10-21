c72f34283e779a16c46a8b352d3065e09120cd20
bluebamus <bluebamus@naver.com>
2024-10-21 15:24:39 +0900

    add templatetags/blog_base_tag.py to remove html and style tags

--- blog/templatetags/blog_base_tag.py
+++ blog/templatetags/blog_base_tag.py
@@ -3,6 +3,8 @@ import logging
 from django import template
 from django.conf import settings
 from django.core.paginator import Page
+import re
+from django.utils.html import strip_tags
 
 logger = logging.getLogger(getattr(settings, "BLOG_LOGGER", "django"))
 register = template.Library()
@@ -23,3 +25,10 @@ def slice_visible_pages(paging: Page):
     min_page = max(min_page, 1)
     max_page = min(min_page + 9, paging.paginator.num_pages)
     return range(min_page, max_page + 1)
+
+
+@register.filter(name="remove_styles")
+def remove_styles(value):
+    # 모든 인라인 스타일 속성 제거
+    no_styles = re.sub(r'style="[^"]*"', "", value)
+    return strip_tags(no_styles)  # 스타일 제거 후 모든 태그 제거
